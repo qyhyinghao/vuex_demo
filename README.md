08d465250ff96f8261d2426b0e1981bec1499cc1
zhengwei1949 <zhengwei1949@qq.com>
2017-09-15 23:02:27 +0800

    a

---
+++ README.md
@@ -0,0 +1,3 @@
+# vuex_demo
+1. 安装vuex `npm install --save vuex`
+2. 
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    {{count}}
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import {mapState} from 'vuex';
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  },
+  computed:{
+    ...mapState(['count'])
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
